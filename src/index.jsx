7094493ee6334b13e94aa3acd083aa9b5829914a
EduardoMontanha <edumontanha14@gmail.com>
2020-09-03 16:12:55 -0300

    Change file extension

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/_main.scss';
+import 'react-notifications/lib/notifications.css';
 
 ReactDOM.render(
   <React.StrictMode>
