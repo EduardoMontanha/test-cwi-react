9fd7e4ef56b35cb40fa7c1daf8f0fb01ebee9093
EduardoMontanha <edumontanha14@gmail.com>
2020-09-03 16:10:35 -0300

    Create basic page structure

---
+++ src/routes/Dragon.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Text from './components/Text';
+
+function Dragon () {
+
+    return (
+        <main id="dragon">
+            {/* <Text pageId="login" tid="title" /> */}
+            <p>dragon</p>
+        </main>
+    );
+}
+
+export default Dragon;
