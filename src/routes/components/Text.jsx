57ee59b8e0777510f036fd573e622d6ec4451099
EduardoMontanha <edumontanha14@gmail.com>
2020-09-03 16:13:18 -0300

    Create component to use translated texts

---
+++ src/routes/components/Text.jsx
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import { LanguageContext } from '../../contexts/LanguageContext';
+
+function Text ({ pageId, tid}) {
+    const languageContext = useContext(LanguageContext);
+    
+    return languageContext.langData[pageId][tid];
+};
+
+export default Text;
