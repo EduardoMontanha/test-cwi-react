e2026b7491f24be02751eb4d5066e6e2ca22de96
EduardoMontanha <edumontanha14@gmail.com>
2020-09-03 22:25:44 -0300

    Finish Header

--- src/routes/components/Header.jsx
+++ src/routes/components/Header.jsx
@@ -1,31 +1,59 @@
 import React, { useContext } from 'react'
 import { Link, Redirect } from 'react-router-dom'
 import AuthContext from '../../contexts/AuthContext';
+import Text from './Text';
+import LanguageSelector from './LanguageSelector';
+import logo from '../../assets/images/logo.png';
 
-function Header () {
+function Header() {
     const auth = useContext(AuthContext);
+    const pageId = "header";
 
     const handleLogout = () => {
         auth.setAuth(false);
         return <Redirect to="/login" />;
     }
 
+    function toggleMenu() {
+        document.querySelector('header').classList.toggle('active');
+    }
+
     return (
         <header>
-            <ul>
-                <li>
-                    <Link to="/dragon">Dragon</Link>
-                </li>
-                <li>
-                    <Link to="/">Dragons</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <button onClick={() => handleLogout()}>Logout</button>
-                </li>
-            </ul>
+            <div className="container">
+                <Link className="logo" to="/"><img src={logo} alt="Logo" /></Link>
+
+                <button className="nav-bar-toggler" onClick={toggleMenu}>
+                    <div id="burger-icon">
+                        <div className="bar bar1"></div>
+                        <div className="bar bar2"></div>
+                        <div className="bar bar3"></div>
+                    </div>
+                </button>
+
+                <nav className="nav-links">
+                    <ul>
+                        <li className="nav-item">
+                            <Link to="/">
+                                <Text pageId={pageId} tid="dragons" />
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/adicionar">
+                                <Text pageId={pageId} tid="add" />
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <button onClick={() => handleLogout()}>
+                                <Text pageId={pageId} tid="logout" />
+                            </button>
+                        </li>
+                        <li className="nav-item">
+                            <LanguageSelector />
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 }
