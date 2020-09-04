4d764eba8c07c38aedaec6d15014acbd13c614e9
EduardoMontanha <edumontanha14@gmail.com>
2020-09-03 23:26:32 -0300

    Add header to private links

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,23 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Route, Redirect } from "react-router-dom";
 import { useContext } from "react";
 import AuthContext from "../contexts/AuthContext";
+import Header from "./components/Header";
 
 function PrivateRoute({ component: Component, ...rest }) {
-  const auth = useContext(AuthContext)
+    const auth = useContext(AuthContext)
 
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        auth.auth ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to="/login" />
-        )
-      }
-    />
-  );
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                auth.auth ? (
+                    <Fragment>
+                        <Header />
+                        <Component {...props} />
+                    </Fragment>
+                ) : (
+                        <Redirect to="/login" />
+                    )
+            }
+        />
+    );
 }
 
 export default PrivateRoute;
