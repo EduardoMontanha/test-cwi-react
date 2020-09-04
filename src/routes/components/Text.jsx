73b2d2cf610214905a4be23a46288e1b5e0da75d
EduardoMontanha <edumontanha14@gmail.com>
2020-09-03 22:29:16 -0300

    Add conditional

--- src/routes/components/Text.jsx
+++ src/routes/components/Text.jsx
@@ -4,7 +4,7 @@ import { LanguageContext } from '../../contexts/LanguageContext';
 function Text ({ pageId, tid}) {
     const languageContext = useContext(LanguageContext);
     
-    return languageContext.langData[pageId][tid];
+    return languageContext.langData[pageId][tid] || '';
 };
 
 export default Text;
