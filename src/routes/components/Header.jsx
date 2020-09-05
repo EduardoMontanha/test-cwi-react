ccf18bed0b179983742b419ef4a14f1834579a4a
EduardoMontanha <edumontanha14@gmail.com>
2020-09-04 21:16:53 -0300

    Change way of showing and hiding menu

--- src/routes/components/Header.jsx
+++ src/routes/components/Header.jsx
@@ -1,12 +1,27 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { Link, Redirect } from 'react-router-dom'
 import AuthContext from '../../contexts/AuthContext';
 import Text from './Text';
 import logo from '../../assets/images/logo.png';
+import useWindowSize from '../../hooks/useWindowSize';
 
 function Header() {
     const auth = useContext(AuthContext);
+    const [isMobile, setIsMobile] = useState(true);
+    const [hideMenu, setHideMenu] = useState(true);
     const pageId = "header";
+
+    useEffect(() => {
+        const size = useWindowSize();
+
+        if (size.width >= 1024) {
+            setIsMobile(false);
+            setHideMenu(false);
+        } else {
+            setIsMobile(true);
+            setHideMenu(true);
+        }
+    });
 
     const handleLogout = () => {
         auth.setAuth(false);
@@ -14,7 +29,13 @@ function Header() {
     }
 
     function toggleMenu() {
-        document.querySelector('header').classList.toggle('active');
+
+        console.log(isMobile, hideMenu, !hideMenu);
+        console.log(size)
+
+        if (isMobile) {
+            setHideMenu(!hideMenu);
+        }
     }
 
     return (
@@ -30,7 +51,7 @@ function Header() {
                     </div>
                 </button>
 
-                <nav className="nav-links">
+                <nav className={`nav-links ${hideMenu ? 'hide' : ''}`}>
                     <ul>
                         <li className="nav-item">
                             <Link to="/" onClick={() => toggleMenu()}>
